9a39a4a46724198156ea020075b62280ff2e247d
Tuukka Haapasalo <tuukka.haapasalo@hiq.fi>
2015-10-14 13:10:46 +0300

    Tuned UI to render labyrinth faster (directly to canvas)

--- src/main/scala/fi/haapatalo/labyrinth/LabyrinthUI.scala
+++ src/main/scala/fi/haapatalo/labyrinth/LabyrinthUI.scala
@@ -3,20 +3,21 @@ package fi.haapatalo.labyrinth
 import Labyrinth.CoordinateExt
 import javafx.application.Application
 import javafx.scene.{ Group, Scene }
-import javafx.scene.effect.BlendMode
+import javafx.scene.canvas.{ Canvas, GraphicsContext }
 import javafx.scene.paint.Color
-import javafx.scene.shape.{ Circle, Line, Rectangle, Shape }
+import javafx.scene.shape.Rectangle
 import javafx.stage.Stage
+import javafx.scene.shape.StrokeLineCap
 
 class LabyrinthUI extends Application {
 
   import Labyrinth.CoordinateExt
 
-  val width = 300
-  val height = 250
+  val width = 950
+  val height = 500
   val roomSize = 1
   val wallSize = 1
-  val doorSize = 1d * roomSize
+  val doorSize = 1 * roomSize
   val imWidth = width * roomSize + (width + 1) * wallSize
   val imHeight = height * roomSize + (height + 1) * wallSize
 
@@ -28,33 +29,53 @@ class LabyrinthUI extends Application {
 
   val labyrinth = Labyrinth(width, height)
 
-  @inline def imx(x: Int): Double = wallSize + x * (roomSize + wallSize) + roomSize / 2d
-  @inline def imy(y: Int): Double = wallSize + (height - y - 1) * (roomSize + wallSize) + roomSize / 2d
+  @inline def imcx(x: Int): Double = imtx(x) + roomSize / 2d
+  @inline def imcy(y: Int): Double = imty(y) + roomSize / 2d
+  @inline def imtx(x: Int): Double = wallSize + x * (roomSize + wallSize)
+  @inline def imty(y: Int): Double = wallSize + (height - y - 1) * (roomSize + wallSize)
 
-  @inline def room(x: Int, y: Int) = circleRoom(x, y)
-  @inline def circleRoom(x: Int, y: Int): Circle = {
-    val r = new Circle(imx(x), imy(y), roomSize.toDouble / 2d, Colors.room)
-    r
-  }
+  @inline def dot(x: Double, y: Double)(implicit g: GraphicsContext) = g.strokeLine(x, y, x, y)
 
-  @inline def door(c1: Labyrinth.Coordinate, dir: Labyrinth.Direction): Shape = {
+  @inline def room(x: Int, y: Int)(implicit g: GraphicsContext) = dotRoom(x, y)
+  @inline def circleRoom(x: Int, y: Int)(implicit g: GraphicsContext) =
+    g.fillOval(imtx(x), imty(y), roomSize, roomSize)
+  @inline def dotRoom(x: Int, y: Int)(implicit g: GraphicsContext) = dot(imcx(x), imcy(y))
+
+  @inline def door(c1: Labyrinth.Coordinate, dir: Labyrinth.Direction)(implicit g: GraphicsContext) = {
     val c2 = c1.to(dir)
-    val l = new Line(imx(c1.x), imy(c1.y), imx(c2.x), imy(c2.y))
+    /*
+    val l = new Line(imcx(c1.x), imcy(c1.y), imcx(c2.x), imcy(c2.y))
     l.setStroke(Colors.wall)
     l.setStrokeWidth(doorSize)
     l.setBlendMode(BlendMode.SCREEN)
-    l
+    l*/
+    g.strokeLine(imcx(c1.x), imcy(c1.y), imcx(c2.x), imcy(c2.y))
   }
 
-  def createRooms = for (x <- 0 until width; y <- 0 until height) yield room(x, y)
-  def createDoors = (for (x <- 0 until width; y <- 0 until height; dir <- Seq(Labyrinth.North, Labyrinth.East)) yield (x, y, dir)).
-    filter(w => !labyrinth.hasWall(w._1, w._2, w._3)).map(w => door((w._1, w._2), w._3))
+  def createRooms(implicit g: GraphicsContext) = {
+    g.setFill(Colors.room)
+    g.setStroke(Colors.wall)
+    g.setLineWidth(roomSize)
+    g.setLineCap(StrokeLineCap.SQUARE)
+    for (x <- 0 until width; y <- 0 until height) room(x, y)
+  }
+
+  def createDoors(implicit g: GraphicsContext) = {
+    g.setStroke(Colors.wall)
+    g.setLineWidth(roomSize)
+    g.setLineCap(StrokeLineCap.SQUARE)
+    (for (x <- 0 until width; y <- 0 until height; dir <- Seq(Labyrinth.North, Labyrinth.East)) yield (x, y, dir)).
+      filter(w => !labyrinth.hasWall(w._1, w._2, w._3)).map(w => door((w._1, w._2), w._3))
+  }
 
   override def start(stage: Stage) {
-    val background = new Rectangle(imWidth, imHeight, Colors.background)
-    val rooms = new Group(createRooms: _*)
-    val doors = new Group(createDoors: _*)
-    val root = new Group(background, rooms, doors)
+    val drawing = new Canvas(imWidth, imHeight)
+    implicit val g = drawing.getGraphicsContext2D
+    g.setFill(Colors.background)
+    g.fillRect(0, 0, imWidth, imHeight)
+    //createRooms
+    createDoors
+    val root = new Group(drawing)
     val scene = new Scene(root, imWidth, imHeight)
 
     stage.setTitle("Labyrinth")
